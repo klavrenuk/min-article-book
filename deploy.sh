979e209f2b448da2aa174115f89fad69d2846978
klavrenuk <klavrenuk@yandex.ru>
2023-05-27 00:22:43 +0500

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:klavrenuk/min-article-book.git main:gh-pages
+git push -f git@github.com:klavrenuk/min-article-book.git master:gh-pages
 
 cd -
